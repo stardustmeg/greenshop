cf4db21aa7d78ccf6e11aa4fe5e2a0f4268aaf52
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-04-24 14:35:13 +0300

    chore: set up ESLint and Stylelint

---
+++ .eslintrc.cjs
@@ -0,0 +1,77 @@
+/**
+ * @type {import('eslint').ESLint.ConfigData}
+ */
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  extends: [
+    "airbnb-base",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:perfectionist/recommended-natural",
+    "prettier",
+  ],
+  overrides: [
+    {
+      extends: ["plugin:@typescript-eslint/disable-type-checked"],
+      files: ["*.js", "*.cjs"],
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    project: true,
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["import", "@typescript-eslint"],
+  root: true,
+  rules: {
+    "@typescript-eslint/consistent-type-assertions": [
+      "error",
+      { assertionStyle: "never" },
+    ],
+    "@typescript-eslint/consistent-type-exports": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "@typescript-eslint/explicit-member-accessibility": [
+      "error",
+      {
+        accessibility: "explicit",
+        overrides: {
+          constructors: "off",
+        },
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-inferrable-types": "error",
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        checksVoidReturn: {
+          arguments: false,
+        },
+      },
+    ],
+    "class-methods-use-this": "off",
+    curly: ["error", "all"],
+    "max-lines-per-function": [
+      "error",
+      { max: 40, skipBlankLines: true, skipComments: true },
+    ],
+    "no-restricted-globals": "off",
+    "no-shadow": "off",
+    "no-use-before-define": "off",
+  },
+  settings: {
+    "import/resolver": {
+      alias: {
+        extensions: [".ts", ".js", ".jsx", ".json"],
+        map: [["@", "./src"]],
+      },
+    },
+  },
+};
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import autoprefixer from 'autoprefixer';
-import path, { resolve } from 'path';
-import checker from 'vite-plugin-checker';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import createSvgSpritePlugin from 'vite-plugin-svg-spriter';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import autoprefixer from "autoprefixer";
+import path, { resolve } from "path";
+import checker from "vite-plugin-checker";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import createSvgSpritePlugin from "vite-plugin-svg-spriter";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-const SRC_PATH = path.resolve(__dirname, 'src');
-const SVG_FOLDER_PATH = path.resolve(SRC_PATH, 'img');
+const SRC_PATH = path.resolve(__dirname, "src");
+const SVG_FOLDER_PATH = path.resolve(SRC_PATH, "img");
 
 export default {
-  base: './',
+  base: "./",
   build: {
     compact: false,
     minify: false,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, './src/index.html'),
+        main: resolve(__dirname, "./src/index.html"),
       },
     },
     sourcemap: true,
@@ -43,6 +43,6 @@ export default {
       typescript: true,
     }),
   ],
-  publicDir: 'assets',
-  root: 'src',
+  publicDir: "assets",
+  root: "src",
 };
