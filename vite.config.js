cda3bb20e1bcd787454df2bd078c5d712a9e144d
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-04-24 13:35:42 +0300

    chore: apply @Kleostro suggestions

--- vite.config.js
+++ vite.config.js
@@ -34,32 +34,6 @@ export default {
       png: {
         quality: 85,
       },
-      svg: {
-        multipass: true,
-        plugins: [
-          {
-            name: 'preset-default',
-            params: {
-              cleanupIDs: {
-                minify: false,
-                remove: false,
-              },
-              convertPathData: false,
-              overrides: {
-                cleanupNumericValues: false,
-                removeViewBox: false, // https://github.com/svg/svgo/issues/1128
-              },
-            },
-          },
-          'sortAttrs',
-          {
-            name: 'addAttributesToSVGElement',
-            params: {
-              attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
-            },
-          },
-        ],
-      },
       webp: {
         quality: 70,
       },
