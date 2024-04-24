a5f3f09508ca96d65262e3b9e0b6ff70a6c9f6dc
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-04-24 14:57:14 +0300

    chore: set up Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,70 +7,64 @@ module.exports = {
     es2022: true,
   },
   extends: [
-    "airbnb-base",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:perfectionist/recommended-natural",
-    "prettier",
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:perfectionist/recommended-natural',
+    'prettier',
   ],
   overrides: [
     {
-      extends: ["plugin:@typescript-eslint/disable-type-checked"],
-      files: ["*.js", "*.cjs"],
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+      files: ['*.js', '*.cjs'],
     },
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
     project: true,
-    sourceType: "module",
+    sourceType: 'module',
     tsconfigRootDir: __dirname,
   },
-  plugins: ["import", "@typescript-eslint"],
+  plugins: ['import', '@typescript-eslint'],
   root: true,
   rules: {
-    "@typescript-eslint/consistent-type-assertions": [
-      "error",
-      { assertionStyle: "never" },
-    ],
-    "@typescript-eslint/consistent-type-exports": "error",
-    "@typescript-eslint/consistent-type-imports": "error",
-    "@typescript-eslint/explicit-function-return-type": "error",
-    "@typescript-eslint/explicit-member-accessibility": [
-      "error",
+    '@typescript-eslint/consistent-type-assertions': ['error', { assertionStyle: 'never' }],
+    '@typescript-eslint/consistent-type-exports': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
       {
-        accessibility: "explicit",
+        accessibility: 'explicit',
         overrides: {
-          constructors: "off",
+          constructors: 'off',
         },
       },
     ],
-    "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/no-inferrable-types": "error",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-inferrable-types': 'error',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
       {
         checksVoidReturn: {
           arguments: false,
         },
       },
     ],
-    "class-methods-use-this": "off",
-    curly: ["error", "all"],
-    "max-lines-per-function": [
-      "error",
-      { max: 40, skipBlankLines: true, skipComments: true },
-    ],
-    "no-restricted-globals": "off",
-    "no-shadow": "off",
-    "no-use-before-define": "off",
+    'class-methods-use-this': 'off',
+    curly: ['error', 'all'],
+    'max-lines-per-function': ['error', { max: 40, skipBlankLines: true, skipComments: true }],
+    'no-restricted-globals': 'off',
+    'no-shadow': 'off',
+    'no-use-before-define': 'off',
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       alias: {
-        extensions: [".ts", ".js", ".jsx", ".json"],
-        map: [["@", "./src"]],
+        extensions: ['.ts', '.js', '.jsx', '.json'],
+        map: [['@', './src']],
       },
     },
   },
