e1a7a58bfe05ce86d49fc194e1899e219d6d6499
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-04-28 15:51:46 +0300

    fix: regex and validate config file

--- .validate-branch-namerc.cjs
+++ .validate-branch-namerc.cjs
@@ -1,12 +1,9 @@
-module.exports = branchNameConfig;
-const branchNameConfig = {
-  errorMsg: 'Please use correct branch name',
+module.exports = {
   pattern: /^(feat|fix|hotfix|chore|refactor|revert|docs|style|test|)\(RSS-ECOMM-\d{1}_\d{2}\)\/[a-z]+[a-zA-Z0-9]*$/,
+  errorMsg: 'Please use correct branch name',
 };
 
 // Branch Name Examples:
 
 // feat(RSS-ECOMM-1_01)/add-new-product // where 1 is sprint number and 01 is issue number
 // fix(RSS-ECOMM-2_15)/add-correct-product // where 2 is sprint number and 15 is issue number
-
-module.exports = branchNameConfig;
