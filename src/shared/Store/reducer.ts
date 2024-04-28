c7d247a01583693f3c52d0da8c069c281509cdca
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-04-28 22:45:30 +0300

    feat: add base Redux-like store

---
+++ src/shared/Store/reducer.ts
@@ -0,0 +1,22 @@
+/* eslint-disable max-lines-per-function */
+import type * as actions from './actions.ts';
+import type { Reducer } from './types.ts';
+
+export interface State {
+  currentUser: null | string;
+}
+
+type InferValueTypes<T> = T extends { [key: string]: infer U } ? U : never;
+
+export type Action = ReturnType<InferValueTypes<typeof actions>>;
+export const rootReducer: Reducer<State, Action> = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'setCurrentUser':
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+    default:
+      return state;
+  }
+};
