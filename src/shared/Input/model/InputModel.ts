db4ef6c829927f6e8bb02cec16b2d9849e388d58
Max <133934232+Kleostro@users.noreply.github.com>
2024-04-29 00:14:15 +0300

    feat: add Input component

---
+++ src/shared/Input/model/InputModel.ts
@@ -0,0 +1,35 @@
+import { IS_DISABLED } from '@/shared/constants/enums.ts';
+
+import InputView from '../view/InputView.ts';
+
+class InputModel {
+  private view: InputView;
+
+  constructor(attrs: Record<string, string>) {
+    this.view = new InputView(attrs);
+  }
+
+  public clear(): void {
+    this.view.getHTML().value = '';
+  }
+
+  public getHTML(): HTMLInputElement {
+    return this.view.getHTML();
+  }
+
+  public getValue(): string {
+    return this.view.getHTML().value;
+  }
+
+  public setDisabled(): boolean {
+    this.view.getHTML().disabled = IS_DISABLED.DISABLED;
+    return this.view.getHTML().disabled;
+  }
+
+  public setEnabled(): boolean {
+    this.view.getHTML().disabled = IS_DISABLED.ENABLED;
+    return this.view.getHTML().disabled;
+  }
+}
+
+export default InputModel;
