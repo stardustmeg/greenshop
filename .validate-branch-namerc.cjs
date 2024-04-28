b7a193ee04ae3716f88379b08ccdbd1bb9368586
Max <133934232+Kleostro@users.noreply.github.com>
2024-04-28 16:58:40 +0300

    chore: update branch name examples

--- .validate-branch-namerc.cjs
+++ .validate-branch-namerc.cjs
@@ -5,5 +5,5 @@ module.exports = {
 
 // Branch Name Examples:
 
-// "feat(RSS-ECOMM-1_01)/add-new-product" // where 1 is sprint number and 01 is issue number
-// "fix(RSS-ECOMM-2_15)/add-correct-product" // where 2 is sprint number and 15 is issue number
+// "feat(RSS-ECOMM-1_01)/addNewProduct" // where 1 is the sprint number and 01 is the issue number
+// "fix(RSS-ECOMM-2_15)/addCorrectProduct" // where 2 is the sprint number and 15 is the issue number
