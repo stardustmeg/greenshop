fd09d743b1499ab6ffacbc778739a3cd881fa59a
Max <133934232+Kleostro@users.noreply.github.com>
2024-05-04 21:46:38 +0300

    feat: add header styles

--- src/widgets/Header/view/HeaderView.ts
+++ src/widgets/Header/view/HeaderView.ts
@@ -1,12 +1,19 @@
+import LinkModel from '@/shared/Link/model/LinkModel.ts';
+import { PAGE_ID } from '@/shared/constants/pages.ts';
+import SVG_DETAILS from '@/shared/constants/svg.ts';
 import TAG_NAME from '@/shared/constants/tags.ts';
 import createBaseElement from '@/shared/utils/createBaseElement.ts';
+import createSVGUse from '@/shared/utils/createSVGUse.ts';
 
 import styles from './headerView.module.scss';
 
 class HeaderView {
   private header: HTMLElement;
 
+  private linkLogo: LinkModel;
+
   constructor() {
+    this.linkLogo = this.createLinkLogo();
     this.header = this.createHTML();
   }
 
@@ -15,7 +22,23 @@ class HeaderView {
       cssClasses: [styles.header],
       tag: TAG_NAME.HEADER,
     });
+
+    this.header.append(this.linkLogo.getHTML());
     return this.header;
+  }
+
+  private createLinkLogo(): LinkModel {
+    this.linkLogo = new LinkModel({
+      attrs: {
+        href: PAGE_ID.DEFAULT_PAGE,
+      },
+      classes: [styles.logo],
+    });
+
+    const svg = document.createElementNS(SVG_DETAILS.SVG_URL, TAG_NAME.SVG);
+    svg.append(createSVGUse(SVG_DETAILS.LOGO));
+    this.linkLogo.getHTML().append(svg);
+    return this.linkLogo;
   }
 
   public getHTML(): HTMLElement {
