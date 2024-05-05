4650639583ca9946566f6f7c10d9154206bef510
Max <133934232+Kleostro@users.noreply.github.com>
2024-05-05 22:24:18 +0300

    feat: add type validation for user

--- src/shared/types/validation/user.ts
+++ src/shared/types/validation/user.ts
@@ -1,21 +1,72 @@
-import type { FormAddress, UserLoginData } from '../user';
+import type { Address, FormAddress, User, UserLoginData } from '../user';
 
 export const isUserLoginData = (data: unknown): data is UserLoginData =>
   typeof data === 'object' &&
   data !== null &&
   'email' in data &&
-  typeof data.email === 'string' &&
   'password' in data &&
+  typeof data.email === 'string' &&
   typeof data.password === 'string';
 
 export const isFormAddress = (data: unknown): data is FormAddress =>
   typeof data === 'object' &&
   data !== null &&
   'city' in data &&
-  typeof data.city === 'string' &&
   'country' in data &&
-  typeof data.country === 'string' &&
   'postalCode' in data &&
-  typeof data.postalCode === 'string' &&
   'streetName' in data &&
+  typeof data.city === 'string' &&
+  typeof data.country === 'string' &&
+  typeof data.postalCode === 'string' &&
   typeof data.streetName === 'string';
+
+export const isAddress = (data: unknown): data is Address =>
+  typeof data === 'object' &&
+  data !== null &&
+  'city' in data &&
+  'country' in data &&
+  'postalCode' in data &&
+  'streetName' in data &&
+  'email' in data &&
+  'firstName' in data &&
+  'id' in data &&
+  'lastName' in data &&
+  'state' in data &&
+  'streetNumber' in data &&
+  typeof data.city === 'string' &&
+  typeof data.country === 'string' &&
+  typeof data.postalCode === 'string' &&
+  typeof data.streetName === 'string' &&
+  typeof data.email === 'string' &&
+  typeof data.firstName === 'string' &&
+  typeof data.id === 'string' &&
+  typeof data.lastName === 'string' &&
+  typeof data.state === 'string' &&
+  typeof data.streetNumber === 'string';
+
+export const isUser = (data: unknown): data is User =>
+  typeof data === 'object' &&
+  data !== null &&
+  'addresses' in data &&
+  'birthDate' in data &&
+  'defaultBillingAddressId' in data &&
+  'defaultShippingAddressId' in data &&
+  'email' in data &&
+  'firstName' in data &&
+  'id' in data &&
+  'lastName' in data &&
+  'locale' in data &&
+  'password' in data &&
+  'version' in data &&
+  Array.isArray(data.addresses) &&
+  data.addresses.every((address) => isAddress(address)) &&
+  typeof data.birthDate === 'string' &&
+  (isAddress(data.defaultBillingAddressId) || data.defaultBillingAddressId === null) &&
+  (isAddress(data.defaultShippingAddressId) || data.defaultBillingAddressId === null) &&
+  typeof data.email === 'string' &&
+  typeof data.firstName === 'string' &&
+  typeof data.id === 'string' &&
+  typeof data.lastName === 'string' &&
+  typeof data.locale === 'string' &&
+  typeof data.password === 'string' &&
+  typeof data.version === 'number';
