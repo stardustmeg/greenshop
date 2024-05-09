81ba57ff2fe3a187cb836a98e8a958931e87978b
Max <133934232+Kleostro@users.noreply.github.com>
2024-05-09 03:20:57 +0300

    fix: layout login form

--- src/widgets/LoginForm/model/LoginFormModel.ts
+++ src/widgets/LoginForm/model/LoginFormModel.ts
@@ -12,6 +12,7 @@ import { INPUT_TYPE, PASSWORD_TEXT } from '@/shared/constants/forms.ts';
 import { MESSAGE_STATUS, SERVER_MESSAGE } from '@/shared/constants/messages.ts';
 import { SIZES } from '@/shared/constants/sizes.ts';
 import { isUserCredentialsData } from '@/shared/types/validation/user.ts';
+import formattedText from '@/shared/utils/formattedText.ts';
 import { greeting } from '@/shared/utils/messageTemplate.ts';
 
 import LoginFormView from '../view/LoginFormView.ts';
@@ -32,8 +33,8 @@ class LoginFormModel {
 
   private getFormData(): UserCredentials {
     const userData: UserCredentials = {
-      email: this.view.getEmailField().getView().getValue(),
-      password: this.view.getPasswordField().getView().getValue(),
+      email: formattedText(this.view.getEmailField().getView().getValue()),
+      password: formattedText(this.view.getPasswordField().getView().getValue()),
     };
 
     this.view.getInputFields().forEach((inputField) => inputField.getView().getInput().clear());
