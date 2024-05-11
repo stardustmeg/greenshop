7353781d2bdbd0d129e17b7010a41e5eeee6970b
Max <133934232+Kleostro@users.noreply.github.com>
2024-05-11 15:30:06 +0300

    feat: expand loder options

--- src/shared/Loader/model/LoaderModel.ts
+++ src/shared/Loader/model/LoaderModel.ts
@@ -1,16 +1,20 @@
-import type { SizesType } from '@/shared/constants/sizes.ts';
+import type { LoaderSizeType } from '@/shared/constants/sizes.ts';
 
 import LoaderView from '../view/LoaderView.ts';
 
 class LoaderModel {
   private view: LoaderView;
 
-  constructor(size: SizesType) {
+  constructor(size: LoaderSizeType) {
     this.view = new LoaderView(size);
   }
 
   public getHTML(): HTMLDivElement {
     return this.view.getHTML();
+  }
+
+  public setAbsolutePosition(): void {
+    this.view.setAbsolutePosition();
   }
 }
 
