6cd7d164a22d709370c1a8f4005fe4c0a946f5a8
YulikK <yulikpoch@gmail.com>
2024-05-13 18:59:27 +0200

    feat: add filter Object, make filer with OR concatenation

--- src/widgets/Catalog/model/CatalogModel.ts
+++ src/widgets/Catalog/model/CatalogModel.ts
@@ -3,7 +3,8 @@ import type ProductFiltersParams from '@/shared/types/productFilters.ts';
 import ProductCardModel from '@/entities/ProductCard/model/ProductCardModel.ts';
 import ProductFiltersModel from '@/features/ProductFilters/model/ProductFiltersModel.ts';
 import getProductModel from '@/shared/API/product/model/ProductModel.ts';
-import addFilter from '@/shared/API/product/utils/filter.ts';
+// import addFilter from '@/shared/API/product/utils/filter.ts';
+import FilterProduct from '@/shared/API/product/utils/filter.ts';
 import { FilterFields, type OptionsRequest } from '@/shared/API/types/type.ts';
 import LoaderModel from '@/shared/Loader/model/LoaderModel.ts';
 import getStore from '@/shared/Store/Store.ts';
@@ -55,16 +56,16 @@ class CatalogModel {
 
   private getSelectedFilters(): OptionsRequest {
     const { category, price, size } = getStore().getState().selectedFilters || {};
-    const filter: OptionsRequest['filter'] = [];
-    category?.forEach((categoryID) => filter.push(addFilter(FilterFields.CATEGORY, categoryID)));
+    const filter = new FilterProduct();
+    category?.forEach((categoryID) => filter.addFilter(FilterFields.CATEGORY, categoryID));
     if (price?.max || price?.min) {
-      filter.push(addFilter(FilterFields.PRICE, price));
+      filter.addFilter(FilterFields.PRICE, price);
     }
     if (size) {
-      filter.push(addFilter(FilterFields.SIZE, size));
+      filter.addFilter(FilterFields.SIZE, size);
     }
 
-    return { filter, limit: 100, sort: { direction: 'desc', field: 'name', locale: 'en' } };
+    return { filter: filter.getFilter(), limit: 100, sort: { direction: 'desc', field: 'name', locale: 'en' } };
   }
 
   private init(): void {
