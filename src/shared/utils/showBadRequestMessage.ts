acb345e5af06869f23ece420114266d7ac272a0f
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-05-14 03:16:07 +0300

    chore: complete merge

--- src/shared/utils/showBadRequestMessage.ts
+++ src/shared/utils/showBadRequestMessage.ts
@@ -1,11 +1,7 @@
 import serverMessageModel from '../ServerMessage/model/ServerMessageModel.ts';
-import getStore from '../Store/Store.ts';
-import { MESSAGE_STATUS, SERVER_MESSAGE } from '../constants/messages.ts';
+import { MESSAGE_STATUS, SERVER_MESSAGE_KEYS } from '../constants/messages.ts';
 
 const showBadRequestMessage = (): boolean =>
-  serverMessageModel.showServerMessage(
-    SERVER_MESSAGE[getStore().getState().currentLanguage].BAD_REQUEST,
-    MESSAGE_STATUS.ERROR,
-  );
+  serverMessageModel.showServerMessage(SERVER_MESSAGE_KEYS.BAD_REQUEST, MESSAGE_STATUS.ERROR);
 
 export default showBadRequestMessage;
