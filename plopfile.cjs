923a204eb22f468b6a9ed1becd91a3f15858bf0f
Max <133934232+Kleostro@users.noreply.github.com>
2024-05-15 11:27:46 +0300

    chore: integrate plop to create component folder

---
+++ plopfile.cjs
@@ -0,0 +1,37 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable func-names */
+module.exports = function (plop) {
+  plop.setGenerator('dynamicStructure', {
+    actions(data) {
+      const { folderName } = data;
+      const upperCaseComponentName = String(folderName)[0].toUpperCase() + String(folderName).slice(1);
+      const lowerCaseComponentName = String(folderName)[0].toLowerCase() + String(folderName).slice(1);
+      return [
+        {
+          path: `${upperCaseComponentName}/model/${upperCaseComponentName}Model.ts`,
+          templateFile: 'templates/NewComponent/model/TemplateModel.ts',
+          type: 'add',
+        },
+        {
+          path: `${upperCaseComponentName}/view/${upperCaseComponentName}View.ts`,
+          templateFile: 'templates/NewComponent/view/TemplateView.ts',
+          type: 'add',
+        },
+        {
+          path: `${upperCaseComponentName}/view/${lowerCaseComponentName}View.module.scss`,
+          templateFile: 'templates/NewComponent/view/templateView.module.scss',
+          type: 'add',
+        },
+      ];
+    },
+    description: 'Генератор для динамической структуры',
+    prompts: [
+      {
+        message: 'Введите имя нового компонента:',
+        name: 'folderName',
+        type: 'input',
+      },
+    ],
+  });
+};
