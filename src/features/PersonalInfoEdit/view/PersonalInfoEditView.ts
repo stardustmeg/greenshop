3bf99bce7449a7330dd7515a421719a9e23218c3
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-05-24 00:25:43 +0300

    feat: implement personal info edit

--- src/features/PersonalInfoEdit/view/PersonalInfoEditView.ts
+++ src/features/PersonalInfoEdit/view/PersonalInfoEditView.ts
@@ -1,24 +1,87 @@
+import InputFieldModel from '@/entities/InputField/model/InputFieldModel.ts';
+import ButtonModel from '@/shared/Button/model/ButtonModel.ts';
+import InputModel from '@/shared/Input/model/InputModel.ts';
+import getStore from '@/shared/Store/Store.ts';
+import { BUTTON_TEXT } from '@/shared/constants/buttons.ts';
+import { EMAIL } from '@/shared/constants/forms/fieldParams.ts';
+import { EMAIL_VALIDATE } from '@/shared/constants/forms/validationParams.ts';
 import createBaseElement from '@/shared/utils/createBaseElement.ts';
 
 import styles from './personalInfoEditView.module.scss';
+import './personalInfoEditView.scss';
 
 class PersonalInfoEditView {
-  private view: HTMLDivElement;
+  private cancelButton: ButtonModel;
+
+  private emailField: InputFieldModel;
+
+  private saveChangesButton: ButtonModel;
+
+  private view: HTMLFormElement;
 
   constructor() {
+    this.emailField = this.createEmailField();
+    this.saveChangesButton = this.createSaveChangesButton();
+    this.cancelButton = this.createCancelButton();
     this.view = this.createHTML();
   }
 
-  private createHTML(): HTMLDivElement {
-    this.view = createBaseElement({
-      cssClasses: [styles.style],
-      tag: 'div',
+  private createCancelButton(): ButtonModel {
+    this.cancelButton = new ButtonModel({
+      classes: [styles.cancelButton],
+      text: BUTTON_TEXT[getStore().getState().currentLanguage].CANCEL,
     });
+    return this.cancelButton;
+  }
+
+  private createEmailField(): InputFieldModel {
+    this.emailField = new InputFieldModel(EMAIL, EMAIL_VALIDATE);
+    return this.emailField;
+  }
+
+  private createHTML(): HTMLFormElement {
+    this.view = createBaseElement({
+      cssClasses: [styles.wrapper, 'modalForm'],
+      tag: 'form',
+    });
+
+    const inputFieldElement = this.emailField.getView().getHTML();
+    const inputHTML = this.emailField.getView().getInput().getHTML();
+    if (inputFieldElement instanceof HTMLLabelElement) {
+      inputFieldElement.classList.add(styles.label, styles.labelEmail);
+      inputHTML.classList.add(styles.input);
+      this.view.append(inputFieldElement);
+    } else if (inputFieldElement instanceof InputModel) {
+      this.view.append(inputFieldElement.getHTML());
+    }
+
+    this.view.append(this.cancelButton.getHTML(), this.saveChangesButton.getHTML());
     return this.view;
   }
 
-  public getHTML(): HTMLDivElement {
+  private createSaveChangesButton(): ButtonModel {
+    this.saveChangesButton = new ButtonModel({
+      classes: [styles.saveChangesButton],
+      text: BUTTON_TEXT[getStore().getState().currentLanguage].SAVE_CHANGES,
+    });
+    this.saveChangesButton.setDisabled();
+    return this.saveChangesButton;
+  }
+
+  public getCancelButton(): ButtonModel {
+    return this.cancelButton;
+  }
+
+  public getEmailField(): InputFieldModel {
+    return this.emailField;
+  }
+
+  public getHTML(): HTMLFormElement {
     return this.view;
+  }
+
+  public getSaveChangesButton(): ButtonModel {
+    return this.saveChangesButton;
   }
 }
 
