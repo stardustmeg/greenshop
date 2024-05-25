e8e5e9008abcff984c75ded5b107354d08f4a471
Max <133934232+Kleostro@users.noreply.github.com>
2024-05-25 03:59:39 +0300

    feat: add progress bar to server message

--- src/shared/ServerMessage/view/ServerMessageView.ts
+++ src/shared/ServerMessage/view/ServerMessageView.ts
@@ -1,18 +1,24 @@
 import type { MessageStatusType, ServerMessageKeysType } from '@/shared/constants/messages.ts';
 
 import getStore from '@/shared/Store/Store.ts';
-import SERVER_MESSAGE_ANIMATE_DETAILS from '@/shared/constants/animations.ts';
+import SERVER_MESSAGE_ANIMATE_DETAILS, {
+  SERVER_MESSAGE_PROGRESS_BAR_ANIMATE_DETAILS_END,
+  SERVER_MESSAGE_PROGRESS_BAR_ANIMATE_DETAILS_START,
+} from '@/shared/constants/animations.ts';
 import { MESSAGE_STATUS, SERVER_MESSAGE } from '@/shared/constants/messages.ts';
 import createBaseElement from '@/shared/utils/createBaseElement.ts';
 
 import styles from './serverMessageView.module.scss';
 
 class ServerMessageView {
+  private progressBar: HTMLDivElement;
+
   private serverMessage: HTMLSpanElement;
 
   private serverWrapper: HTMLDivElement;
 
   constructor() {
+    this.progressBar = this.createProgressBar();
     this.serverMessage = this.createServerMessage();
     this.serverWrapper = this.createHTML();
   }
@@ -23,9 +29,18 @@ class ServerMessageView {
       tag: 'div',
     });
 
-    this.serverWrapper.append(this.serverMessage);
+    this.serverWrapper.append(this.serverMessage, this.progressBar);
     document.body.append(this.serverWrapper);
     return this.serverWrapper;
+  }
+
+  private createProgressBar(): HTMLDivElement {
+    this.progressBar = createBaseElement({
+      cssClasses: [styles.progressBar],
+      tag: 'div',
+    });
+
+    return this.progressBar;
   }
 
   private createServerMessage(): HTMLSpanElement {
@@ -41,6 +56,20 @@ class ServerMessageView {
     this.serverWrapper.animate(SERVER_MESSAGE_ANIMATE_DETAILS.params, {
       duration: SERVER_MESSAGE_ANIMATE_DETAILS.duration,
       easing: SERVER_MESSAGE_ANIMATE_DETAILS.easing,
+    });
+
+    this.progressBar.animate(SERVER_MESSAGE_PROGRESS_BAR_ANIMATE_DETAILS_START.params, {
+      delay: SERVER_MESSAGE_PROGRESS_BAR_ANIMATE_DETAILS_START.delay,
+      duration: SERVER_MESSAGE_PROGRESS_BAR_ANIMATE_DETAILS_START.duration,
+      easing: SERVER_MESSAGE_PROGRESS_BAR_ANIMATE_DETAILS_START.easing,
+      fill: 'forwards',
+    });
+
+    this.progressBar.animate(SERVER_MESSAGE_PROGRESS_BAR_ANIMATE_DETAILS_END.params, {
+      delay: SERVER_MESSAGE_PROGRESS_BAR_ANIMATE_DETAILS_END.delay,
+      duration: SERVER_MESSAGE_PROGRESS_BAR_ANIMATE_DETAILS_END.duration,
+      easing: SERVER_MESSAGE_PROGRESS_BAR_ANIMATE_DETAILS_END.easing,
+      fill: 'forwards',
     });
     return true;
   }
