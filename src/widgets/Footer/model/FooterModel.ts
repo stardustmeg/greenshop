88ef755703f28a61b06d9d96f11b9da76a6519ca
Max <133934232+Kleostro@users.noreply.github.com>
2024-05-25 18:45:53 +0300

    fix: delete navigation from footer

--- src/widgets/Footer/model/FooterModel.ts
+++ src/widgets/Footer/model/FooterModel.ts
@@ -1,20 +1,7 @@
-import NavigationModel from '@/entities/Navigation/model/NavigationModel.ts';
-
 import FooterView from '../view/FooterView.ts';
 
 class FooterModel {
-  private navigation = new NavigationModel();
-
   private view = new FooterView();
-
-  constructor() {
-    this.init();
-  }
-
-  private init(): boolean {
-    this.getHTML().append(this.navigation.getHTML());
-    return true;
-  }
 
   public getHTML(): HTMLElement {
     return this.view.getHTML();
