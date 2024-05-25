1d579400dfc99a780c2bb4ed47a70e50f9d7e5ac
Meg G <146496794+stardustmeg@users.noreply.github.com>
2024-05-25 15:33:09 +0300

    chore(RSS-ECOMM-4_00): update sprint number (#309)
    chore: update sprint number

--- .validate-branch-namerc.cjs
+++ .validate-branch-namerc.cjs
@@ -7,3 +7,5 @@ module.exports = {
 
 // "feat(RSS-ECOMM-1_01)/addNewProduct" // where 1 is the sprint number and 01 is the issue number
 // "fix(RSS-ECOMM-2_15)/addCorrectProduct" // where 2 is the sprint number and 15 is the issue number
+// "chore(RSS-ECOMM-3_01)/addNewProduct" // where 3 is the sprint number and 01 is the issue number
+// "refactor(RSS-ECOMM-4_01)/addNewProduct" // where 4 is the sprint number and 01 is the issue number
