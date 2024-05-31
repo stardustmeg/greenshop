8279ced12fb499554734543d31e80f824eb72e0d
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-05-31 23:46:28 +0300

    refactor: break down into smaller functions

--- src/shared/utils/setPageTitle.ts
+++ src/shared/utils/setPageTitle.ts
@@ -1,24 +1,28 @@
 import { PAGE_ID } from '../constants/pages.ts';
 import { appTitle } from './messageTemplates.ts';
 
-const PROJECT_TITLE = import.meta.env.VITE_APP_PROJECT_TITLE;
-const NEXT_SEGMENT = import.meta.env.VITE_APP_NEXT_SEGMENT;
-const PATH_SEGMENTS_TO_KEEP = import.meta.env.VITE_APP_PATH_SEGMENTS_TO_KEEP;
+const {
+  VITE_APP_NEXT_SEGMENT: NEXT_SEGMENT,
+  VITE_APP_PATH_SEGMENTS_TO_KEEP: PATH_SEGMENTS_TO_KEEP,
+  VITE_APP_PROJECT_TITLE: PROJECT_TITLE,
+} = import.meta.env;
 
-const setPageTitle = (currentPage: string, hasRoute: boolean): void => {
+const getPageTitle = (currentPage: string, hasRoute: boolean): string => {
   let currentPageTitle: string;
 
   if (hasRoute) {
-    if (currentPage === PAGE_ID.DEFAULT_PAGE) {
-      currentPageTitle = PAGE_ID.MAIN_PAGE.slice(PATH_SEGMENTS_TO_KEEP, -NEXT_SEGMENT);
-    } else {
-      currentPageTitle = currentPage.slice(PATH_SEGMENTS_TO_KEEP, -NEXT_SEGMENT);
-    }
+    currentPageTitle = currentPage === PAGE_ID.DEFAULT_PAGE ? PAGE_ID.MAIN_PAGE : currentPage;
   } else {
-    currentPageTitle = PAGE_ID.NOT_FOUND_PAGE.slice(PATH_SEGMENTS_TO_KEEP, -NEXT_SEGMENT);
+    currentPageTitle = PAGE_ID.NOT_FOUND_PAGE;
   }
 
-  document.title = appTitle(PROJECT_TITLE, currentPageTitle);
+  const trimmedTitle = currentPageTitle.slice(PATH_SEGMENTS_TO_KEEP, -NEXT_SEGMENT);
+
+  return appTitle(PROJECT_TITLE, trimmedTitle);
+};
+
+const setPageTitle = (currentPage: string, hasRoute: boolean): void => {
+  document.title = getPageTitle(currentPage, hasRoute);
 };
 
 export default setPageTitle;
