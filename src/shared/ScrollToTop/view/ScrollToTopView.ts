aa1787ef45d181eff89421d7afd447d07db0e142
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-06-03 01:59:33 +0300

    feat: implement scrollToTop button

---
+++ src/shared/ScrollToTop/view/ScrollToTopView.ts
@@ -0,0 +1,44 @@
+import ButtonModel from '@/shared/Button/model/ButtonModel.ts';
+import getStore from '@/shared/Store/Store.ts';
+import observeStore, { selectCurrentLanguage } from '@/shared/Store/observer.ts';
+import { SCROLL_TO_TOP_THRESHOLD } from '@/shared/constants/common.ts';
+import SVG_DETAILS from '@/shared/constants/svg.ts';
+import TOOLTIP_TEXT from '@/shared/constants/tooltip.ts';
+import createSVGUse from '@/shared/utils/createSVGUse.ts';
+
+import styles from './scrollToTopView.module.scss';
+
+class ScrollToTopView {
+  private button: ButtonModel;
+
+  constructor() {
+    this.button = this.createButton();
+  }
+
+  private createButton(): ButtonModel {
+    this.button = new ButtonModel({
+      classes: [styles.scrollToTopButton],
+      title: TOOLTIP_TEXT[getStore().getState().currentLanguage].SCROLL_TO_TOP,
+    });
+
+    const svg = document.createElementNS(SVG_DETAILS.SVG_URL, 'svg');
+    svg.append(createSVGUse(SVG_DETAILS.ARROW_UP));
+    this.button.getHTML().append(svg);
+
+    observeStore(selectCurrentLanguage, () => {
+      this.button.getHTML().title = TOOLTIP_TEXT[getStore().getState().currentLanguage].SCROLL_TO_TOP;
+    });
+
+    return this.button;
+  }
+
+  public getHTML(): HTMLButtonElement {
+    return this.button.getHTML();
+  }
+
+  public toggleVisibility(): void {
+    this.button.getHTML().classList.toggle(styles.hidden, window.scrollY <= SCROLL_TO_TOP_THRESHOLD);
+  }
+}
+
+export default ScrollToTopView;
