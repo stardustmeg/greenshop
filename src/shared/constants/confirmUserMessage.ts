c32d5e15073cab9fe3b367789096309ccf25487e
YulikK <yulikpoch@gmail.com>
2024-06-03 21:19:27 +0200

    feat: add confirm and link

--- src/shared/constants/confirmUserMessage.ts
+++ src/shared/constants/confirmUserMessage.ts
@@ -1,9 +1,11 @@
 export const USER_MESSAGE = {
   en: {
+    CLEAR_CART: 'Are you sure you want to clear the cart?',
     CONFIRM: 'Are you sure you want to proceed?',
     DELETE_ADDRESS: 'Are you sure you want to delete this address?',
   },
   ru: {
+    CLEAR_CART: 'Вы уверены, что хотите очистить корзину?',
     CONFIRM: 'Вы уверены, что хотите продолжить?',
     DELETE_ADDRESS: 'Вы уверены, что хотите удалить этот адрес?',
   },
